4b6ff2b56d9cc093bcd8dde870a94c2b1cf5c6d2
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2017-03-28 10:49:38 -0500

    ultimo

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
+/*
     //ARRAYS
     int score [5];
     //arrays de 5 elemento , va desde 0 hasta 4
+
+
     score [0] = 6;
     score [1] = 7;
     score [2] = 12;
@@ -18,5 +21,24 @@ int main()
     cout << "el elemento 4 en la posicion : 3 ...es igual a : " << score [3] << endl;
     cout << "el elemento 5 en la posicion : 4 ...es igual a : " << score [4] << endl;
 
+    **************************************************************************
+    */
+    int score [6];
+    int c = 5;
+    score [0] = c++;
+    score [1] = c++;
+    score [2] = c++;
+    score [3] = c++;
+    score [4] = c++;
+    score [5] = c++;
+
+    c =5;
+
+    cout << "el arregclo es: " << score [c--] << endl;
+    cout << "el arregclo es: " << score [c--] << endl;
+    cout << "el arregclo es: " << score [c--] << endl;
+    cout << "el arregclo es: " << score [c--] << endl;
+    cout << "el arregclo es: " << score [c--] << endl;
+    cout << "el arregclo es: " << score [c--] << endl;
     return 0;
 }
