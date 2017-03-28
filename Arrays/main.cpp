c04400e996dac7f46f710873925ebba5048c1cb0
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2017-03-28 10:41:31 -0500

    al fin domine repositorios

---
+++ Arrays/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //ARRAYS
+    int score [5];
+    //arrays de 5 elemento , va desde 0 hasta 4
+    score [0] = 6;
+    score [1] = 7;
+    score [2] = 12;
+    score [3] = 56;
+    score [4] = 9;
+    cout << "el elemento 1 en la posicion : 0 ...es igual a : " << score [0] << endl;
+    cout << "el elemento 2 en la posicion : 1 ...es igual a : " << score [1] << endl;
+    cout << "el elemento 3 en la posicion : 2 ...es igual a : " << score [2] << endl;
+    cout << "el elemento 4 en la posicion : 3 ...es igual a : " << score [3] << endl;
+    cout << "el elemento 5 en la posicion : 4 ...es igual a : " << score [4] << endl;
+
+    return 0;
+}
--- Intercambio de Variables/main.cpp
+++ Intercambio de Variables/main.cpp
@@ -4,38 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    float a_cuadrado;
-    float a_circulo;
-    float a_rectangulo;
-    float pi ;
-    float l_cuadrado;
-    float radio_circulo;
-    float alt_rectangulo;
-    float base_rectangulo;
+    int x;
+    int y;
+    x = 10;
+    y = 20;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
 
-    pi = 3.14;
+    x = x + y;
+    y = x - y;
+    x = x - y;
 
-    cout << "Dame un lado del cuadrado";
-    cin >> l_cuadrado;
-    cout << "Area del cuadrado = " << l_cuadrado << "**" << l_cuadrado << endl;
-    a_cuadrado = l_cuadrado*l_cuadrado;
-    cout << "Area del cuadrado = " << a_cuadrado << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
 
-    cout << "Dame el radio del circulo";
-    cin >> radio_circulo;
-    cout << "radio del circulo = " << radio_circulo << "**" << radio_circulo << "*" << pi << endl;
-    a_circulo= radio_circulo*radio_circulo*pi;
-    cout << "area del circulo = " << a_circulo << endl;
-
-
-    cout << "Dame la altura del rectangulo" ;
-    cin >> alt_rectangulo;
-    cout << "Dame la base del rectangulo";
-    cin >> base_rectangulo;
-    cout << "area del rectangulo = " << alt_rectangulo << "*" << base_rectangulo << endl;
-    a_rectangulo = alt_rectangulo * base_rectangulo;
-    cout << "area del rectangulo = " << a_rectangulo ;
-    return 0;
 }
-
