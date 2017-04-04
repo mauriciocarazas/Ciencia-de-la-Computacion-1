60da3fad0c994a3a61fbe35a687541d435493ca6
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2017-04-04 10:37:10 -0500

    terminar tarea

---
+++ Numeros primos/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //HACER UNA FUNCION QUE IMPRIMA LOS NUMEROS PRIMOS MENORES A N
+
+
+    cout << "Dame el limite de la lista" << endl;
+    cin >> max;
+
+
+    for (int i=2 ; i < max ; i++){
+        bool isPrime = true;
+        for (int j=2 ; j<1 ; j++){
+            ig
+        }
+
+
+    // HACER UNA FUNCION QUE INDIQUE QUE NUMERO PRIMO ES N
+    // POR EJEMPLO SI YO PONGO 8 ME DARIA EL OCTAVO PRIMO
+    return 0;
+}
