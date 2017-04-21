0c6aa46a76ed5f260e984e95b182f9e97b0cc6c5
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-04-21 11:24:03 -0500

    examen en clase 21-04-17

---
+++ Examen 21-04/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+//2° PREGUNTA
+
+int sumar(int a[],int n){
+    if (n==1){
+        return a[0];
+    }
+    else{
+        return a [n-1]+ sumar(a,n-1);
+    }
+}
+
+//3° PREGUNTA
+
+void multiplicacion( int matriz1[][m], int matriz2[][p],n){
+    int nuevo[][]
+    for ( int k = 0; k < m; k++){
+        for ( int j = 0; j < p; j++)
+
+    }
+
+
+
+int main(){
+//1
+    int arreglo[4] = {1,2,3,4};
+    cout << sumar (arreglo,4);
+
+//2
+    int matriz1[][]={{1,2},{3,4}};
+    int matriz1[][]={{5,6},{7,8}};
+
+
+//3
+
+
+return 0;
+}
