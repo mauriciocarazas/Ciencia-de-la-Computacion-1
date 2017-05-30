29c4a4db03ed32c04974bbcaef20b845eeafc4c2
mauriciocarazas <mauriciocarazas9812@gmail.com>
2017-05-30 10:00:37 -0500

    asus 30/05

--- Clases Abstractas/main.cpp
+++ Clases Abstractas/main.cpp
@@ -12,17 +12,17 @@ int main() {
 	Persona p1("luis ",71213933, c1);
 	Gerente g1("mateo ", 711232124,c2,"Licenciado", 78123);
 	Conserje d1("marcos ", 87465462,c3,"UCSP", 100);
-	Persona *arr = new Persona[3];
+	/*Persona *arr = new Persona[3];
 	arr[0] = p1;
 	arr[1] =g1;
 	arr[2] = d1;
 	for (int i = 0;i < 3;i++) {
-		cout << arr[i].mostrarDatos() << endl;
+		cout << arr[i]->mostrarDatos() << endl;
 	}
 	//cout << g1.mostrarDatos() << endl;
 	//cout << c1.mostrarDatos() << endl;
 
 
-	system("pause");
+	*/
 	return 0;
 }
